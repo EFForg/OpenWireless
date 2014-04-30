f5b75166a0be0cf7c11c269a7379c40d00624d14
raychatter <rachel.e.walker@gmail.com>
2014-04-30 13:53:04 -0700

    Yay the style is black now! How pretty! Also the changepassword form is fixed

--- app/js/checkPassword.js
+++ app/js/checkPassword.js
@@ -9,25 +9,25 @@ function checkForm()
     if($("#newPassword").val() == " ") {
         alert("Please enter a password!");
         $("#newPassword").focus();
-        return false;
+        return true;
     }
 
     if(!checkPassword($("#newPassword").val())) {
         alert("The password you have entered is not valid!");
         $("#newPassword").focus();
-        return false;
+        return true;
     }
 
     if($("#retypePassword").val() == " ") {
         alert("Please re-enter your password");
         $("#retypePassword").focus();
-        return false;
+        return true;
     }
 
     if($("#newPassword").val() != $("#retypePassword").val()) {
        alert("The passwords do not match!");
        $("#retypePassword").focus();
-       return false;
+       return true;
     }
 
     function changePasswordSuccess() {
@@ -38,5 +38,5 @@ function checkForm()
     var changePasswordRequest = createJsonRequest("user.setpasswd", ["root", $("#newPassword").val()]);
     createAjaxRequest(changePasswordUrl, changePasswordRequest, changePasswordSuccess);
 
-    return true;
+    return false;
 }
