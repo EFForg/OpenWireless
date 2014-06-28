f65d86c0b85a71a4d5354f1e1212a5ef88a07355
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2014-06-28 11:50:41 -0400

    Fix error handling in common.py

--- routerapi/common.py
+++ routerapi/common.py
@@ -9,8 +9,9 @@ def render_error(err, status=403):
 
 def exception_handler(exc_type, exc_obj, exc_tb):
   error_text = '%s: %s' % (exc_type.__name__, exc_obj.__str__())
-  print 'Status: 500 Internal Server Error\r\n'
-  print 'Content-Type: application/json\r\n'
+  print 'Status: 500 Internal Server Error'
+  print 'Content-Type: application/json'
+  print
   print json.JSONEncoder().encode({"error": error_text})
 
 sys.excepthook = exception_handler
