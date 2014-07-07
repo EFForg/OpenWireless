35d9643b51f6bfd513a5f0751d4dbcc358ae30b7
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2014-07-07 14:23:49 -0400

    Include requirement to update templates.js in run-tests.

--- run-tests.sh
+++ run-tests.sh
@@ -5,6 +5,12 @@
 #
 # ln -s ../../run-tests.sh .git/hooks/pre-push
 #
+JS=app/js/templates.js
+TEMPLATES=app/templates
+if [ ! -z "`find $TEMPLATES -newer $JS`" ] ; then
+  echo "Error: templates.js out-of-date. Run ./make-templates.sh"
+  exit 1
+fi
 pip install --user -qr requirements.txt
 python -m unittest discover -s test/ -p '*_test.py'
 npm install
