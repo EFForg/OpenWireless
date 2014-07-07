1e3ec47cb8b7430c6a82bf74aab9a39043f177d1
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2014-07-07 12:58:50 -0400

    Add instructions to make running tests a pre-push hook

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,10 @@
 #!/bin/bash -e
+#
+# Run the tests for OpenWireless. We highly recommend that you
+# make this a git pre-push hook like so:
+#
+# ln -s ../../run-tests.sh .git/hooks/pre-push
+#
 pip install --user -qr requirements.txt
 python -m unittest discover -s test/ -p '*_test.py'
 npm install
