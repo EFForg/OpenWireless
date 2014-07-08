be89919ac2db20eb26b64948a5f605725cbe5e50
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2014-07-08 16:03:52 -0400

    Check for nulls in UCI calls.

--- routerapi/uci.py
+++ routerapi/uci.py
@@ -19,6 +19,11 @@ def commit(*args):
   args.insert(0, "commit")
   run(args)
 
+def check_nulls(string):
+  if string.find('\00') != -1:
+    raise Exception('Invalid input: contains null bytes')
+
 def run(args_list):
   args_list.insert(0, uci_path)
+  map(check_nulls, args_list)
   return check_output(args_list).strip()
