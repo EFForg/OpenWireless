592a9c9d77924cf8b1226499bc1ea480da77ac11
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2014-07-08 16:27:18 -0400

    Faster external IP fetch from myexternalip.com.

--- routerapi/ip_address_retriever.py
+++ routerapi/ip_address_retriever.py
@@ -1,17 +1,16 @@
-import socket, fcntl, struct, re, string
+import socket, fcntl, struct
 from subprocess import check_output
 
 def get_internal_ip_address(interface_name):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    return socket.inet_ntoa(fcntl.ioctl(
-        s.fileno(),
-        0x8915,
-        struct.pack('256s', interface_name[:15])
-    )[20:24])
+    try:
+      return socket.inet_ntoa(fcntl.ioctl(
+          s.fileno(),
+          0x8915,
+          struct.pack('256s', interface_name[:15])
+      )[20:24])
+    except IOError:
+      return '?.?.?.?'
 
 def get_external_ip_address():
-    parser = re.compile('<strong id="ip_address">(\d+).(\d+).(\d+).(\d+)</strong>')
-    ip_page = check_output(["/usr/bin/wget", "-O-", "ifconfig.me"])
-    ip_tuple = parser.findall(ip_page)[0]
-    return ".".join(ip_tuple)
-
+    return check_output(["/usr/bin/wget", "-qO-", "myexternalip.com/raw"])
