3a670f4dbcc6277f5fc031a005abfcb05be2a391
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2014-07-14 16:10:20 -0400

    install-dev-dependencies.sh

---
+++ install-dev-dependencies.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# On Ubuntu and similar systems, install packages that are necessary and/or
+# useful to build and debug OpenWireless.
+sudo apt-get install inotify-tools lighttpd nodejs npm python2.7 python-pip
+cd $(dirname $0)
+pip install --user -qr requirements.txt
+npm install
