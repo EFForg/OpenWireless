0872bcc3aefda93a17480ec88376b7ffd5768ded
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2014-07-15 14:57:14 -0400

    Use new run.py module to run commands.
    Make it easier to mock out commands for local-lighttpd.

---
+++ routerapi/run.py
@@ -0,0 +1,8 @@
+"""
+A thin wrapper around subprocess.check_output. This makes it easy to mock out
+command execution when running with local lighttpd.
+"""
+import subprocess
+
+def check_output(arg_list):
+  return subprocess.check_output(arg_list)
