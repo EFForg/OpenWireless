7caea1f01acbc3b255583bc5f9c7ccb4bd31e0f6
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2014-07-15 09:55:08 -0400

    Put local-lighttpd etc in a directory.

--- local-lighttpd/run-local-lighttpd.sh
+++ local-lighttpd/run-local-lighttpd.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -ex
 DIR=$(dirname $0)
 cd $DIR
-echo "Access the web UI on https://localhost:8843/ or http://localhost:8888/"
+rm -rf etc
+mkdir etc
+echo "Access the web UI on https://localhost:8443/ or http://localhost:8888/"
 exec lighttpd -D $DIR -f local-lighttpd.conf
