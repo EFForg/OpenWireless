a7a66870d353a8920ac63c99f0a26b5336f79d87
Jeremy Gillula <jeremy@eff.org>
2014-07-16 13:15:15 -0700

    Now reseting the wifi when the appropriate setting changes

--- local-lighttpd/run-local-lighttpd.sh
+++ local-lighttpd/run-local-lighttpd.sh
@@ -4,5 +4,6 @@ cd $DIR
 rm -rf etc
 mkdir -m 0700 -p etc/auth
 mkdir -m 0700 -p etc/dropbear
+echo '{"sqm.ge00.download": "0", "sqm.ge00.upload": "0"}' > etc/uci.json
 echo "Access the web UI on https://localhost:8443/ or http://localhost:8888/"
 exec lighttpd -D $DIR -f local-lighttpd.conf
