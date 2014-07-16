48ae04e831575368e4ddef5ede6ca651e027b193
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2014-07-16 12:14:32 -0400

    Improved detection of templates needing update.

--- run-tests.sh
+++ run-tests.sh
@@ -5,13 +5,14 @@
 #
 # ln -s ../../run-tests.sh .git/hooks/pre-push
 #
-JS=app/js/templates.js
-TEMPLATES=app/templates
-if [ ! -z "`find $TEMPLATES -newer $JS`" ] ; then
+
+# Install required packages
+npm install
+pip install --user -qr requirements.txt
+
+if ! ./make-templates.sh --is-updated ; then
   echo "Error: templates.js out-of-date. Run ./make-templates.sh"
   exit 1
 fi
-pip install --user -qr requirements.txt
 python -m unittest discover -s test/ -p '*_test.py'
-npm install
 npm test
