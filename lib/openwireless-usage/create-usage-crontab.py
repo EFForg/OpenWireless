622f225d4b4353b26f5ae135acc45ede5dd233f6
Loos <louisknapp@gmail.com>
2014-07-17 16:04:42 -0500

    JR & LK - add cron job to run accumulate bytes 1x/hr

---
+++ lib/openwireless-usage/create-usage-crontab.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+""" Install a crontab that checks if the router has been reset and updates openwireless usage """
+
+import os
+import random
+import subprocess
+
+CRONTABS = '/var/spool/cron/crontabs'
+try:
+    os.makedirs(CRONTABS)
+except OSError:
+    pass
+if not os.path.isfile(os.path.join(CRONTABS, 'root')):
+    cronline = '* 1 * * * python /www/cgi-bin/routerapi/accumulate_bytes\n'
+    subprocess.Popen(['/usr/bin/crontab', '-'],
+      stdin = subprocess.PIPE).communicate(cronline)
