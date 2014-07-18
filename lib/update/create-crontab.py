b3a82fdd5adc48d5f0468721e9fcdafca7f9cd4a
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2014-07-18 09:49:19 -0400

    Combine usage crontab with update-checking crontab

--- lib/update/create-crontab.py
+++ lib/update/create-crontab.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 """
-Install a crontab that checks for updates at a random minute each hour.
+Install a crontab that:
+  - stores accumulated bandwidth usage at a certain minute each hour,
+    randomly chosen at first boot.
+  - checks for system updates a minute after that.
 """
 import os
 import random
@@ -11,8 +14,18 @@ try:
     os.makedirs(CRONTABS)
 except OSError:
     pass
-minute = int(random.uniform(0, 60))
+# Choose a random minute so that all routers don't check for updates
+# simultaenous. Choose up to 58, so that we can safely do minute + 1.
+minute = int(random.uniform(0, 58))
 if not os.path.isfile(os.path.join(CRONTABS, 'root')):
-    cronline = '%d * * * * python /lib/update/update.py\n' % minute
+    # Temporary hack: We would like to check for updates hourly, then install
+    # them at a time convenient for the user. Right now the update metadata is
+    # triggering an update every time it is fetched, so we only want to check
+    # for updates a maximum of once per day. We check at 1200 UTC, which is 5am
+    # EDT / 2am PDT.
+    crontab = ("""
+%d * * * * python /www/cgi-bin/routerapi/accumulate_bytes
+%d 12 * * * python /lib/update/update.py
+""") % (minute, minute + 1)
     subprocess.Popen(['/usr/bin/crontab', '-'],
-      stdin = subprocess.PIPE).communicate(cronline)
+      stdin = subprocess.PIPE).communicate(crontab)
