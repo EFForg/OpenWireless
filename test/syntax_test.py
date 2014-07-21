891e9668cb19c202a231c29693f33c4a331f5e91
Harvey Hunt <harveyhuntnexus@gmail.com>
2014-07-21 16:38:46 +0100

    Fixed the indentation on all of the test files.

--- test/syntax_test.py
+++ test/syntax_test.py
@@ -5,34 +5,34 @@ import sys
 
 
 routerapi = os.path.join(
-  os.path.dirname(os.path.realpath(__file__)),
-  '..', 'routerapi')
+    os.path.dirname(os.path.realpath(__file__)),
+    '..', 'routerapi')
 
 js = os.path.join(
-  os.path.dirname(os.path.realpath(__file__)),
-  '..', 'app/js')
+    os.path.dirname(os.path.realpath(__file__)),
+    '..', 'app/js')
 
 class TestCompiles(unittest.TestCase):
-  """Verify that every Python file in routerapi compiles."""
-  def test_compile(self):
-    for filename in os.listdir(routerapi):
-      # Exclude dotfiles (e.g. Vim swap files) and *c (e.g. .pyc, or
-      # fooc for script foo.
-      if not filename.startswith('.') and not filename.endswith('c'):
-        with open(os.path.join(routerapi, filename), 'r') as f:
-          contents = f.read()
-          compile(contents, filename, 'exec')
-          self.assertFalse('\t' in contents,
-            msg = 'Tab found in %s, use spaces instead.' % filename)
-          self.assertFalse('\n  ' in contents,
-                msg = 'Indent of 2 spaces in %s, use multiple of 4 instead.' % filename)
+    """Verify that every Python file in routerapi compiles."""
+    def test_compile(self):
+        for filename in os.listdir(routerapi):
+            # Exclude dotfiles (e.g. Vim swap files) and *c (e.g. .pyc, or
+            # fooc for script foo.
+            if not filename.startswith('.') and not filename.endswith('c'):
+                with open(os.path.join(routerapi, filename), 'r') as f:
+                    contents = f.read()
+                    compile(contents, filename, 'exec')
+                    self.assertFalse('\t' in contents,
+                                    msg = 'Tab found in %s, use spaces instead.' % filename)
+                    self.assertFalse('\n  ' in contents,
+                                    msg = 'Indent of 2 spaces in %s, use multiple of 4 instead.' % filename)
 
-  def test_notabs_js(self):
-    for filename in os.listdir(js):
-      if not filename.startswith('.') and not filename == 'templates.js':
-        with open(os.path.join(js, filename), 'r') as f:
-          self.assertFalse('\t' in f.read(),
-            msg = 'Tab found in %s, use spaces instead.' % filename)
+    def test_notabs_js(self):
+        for filename in os.listdir(js):
+            if not filename.startswith('.') and not filename == 'templates.js':
+                with open(os.path.join(js, filename), 'r') as f:
+                    self.assertFalse('\t' in f.read(),
+                        msg = 'Tab found in %s, use spaces instead.' % filename)
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
