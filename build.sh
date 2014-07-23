d89e7fcc1cea5a7a05871b933771a449da4e7cd2
Ranga Krishnan <rk@selwastor.com>
2014-07-23 09:27:13 -0700

    Initial commit of build script

---
+++ build.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+  
+  IMAGE="releases/openwireless-openwrt-squashfs-sysupgrade.bin"
+
+  git submodule init
+  git submodule update
+
+  #build  router image
+  echo "building openwireless"
+
+  #copy the config (kernel + packages)
+  cp OWrt/config-OWrt cerowrt/.config
+
+  #setup the package building system
+  cd cerowrt
+
+  ./scripts/feeds update 
+  #copy over python package
+  cp -r ../ow-python/python-mini-eff ./feeds/oldpackages/lang/
+  ./scripts/feeds update -a
+  #./scripts/feeds install libnettle
+  #./scripts/feeds install python-mini-eff
+  #./scripts/feeds install gnupg
+  #./scripts/feeds install tor 
+
+  #customize the file system
+  ./sendToBuild
+
+  echo "building cerowrt"
+  make
+
+  mkdir -p releases
+  mv cerowrt/bin/ar71xx/openwrt-ar71xx-generic-wndr3800-squashfs-sysupgrade.bin $IMAGE
+  
+  if ! [ -f $IMAGE ]; then
+    echo "ERROR: For some reason $IMAGE failed to generate!"
+    exit 1
+  fi
+
