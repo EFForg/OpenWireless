2ad1b64d4aa7377eb01401e205613f95f39c5d6e
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2014-07-26 19:45:38 -0400

    Provide more default values for local lighttpd.

--- local-lighttpd/run-local-lighttpd.sh
+++ local-lighttpd/run-local-lighttpd.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -ex
+#!/bin/bash -ex
 DIR=$(dirname $0)
 cd $DIR
 HTTP_PORT=${HTTP_PORT:-8000}
@@ -7,7 +8,16 @@ ETC=port-$HTTP_PORT-etc
 rm -rf $ETC
 mkdir -m 0700 -p $ETC/auth
 mkdir -m 0700 -p $ETC/dropbear
-echo '{"sqm.ge00.download": "0", "sqm.ge00.upload": "0"}' > $ETC/uci.json
+touch $ETC/dropbear/authorized_keys
+cat <<EOF > $ETC/uci.json
+{
+  "sqm.ge00.download": "0",
+  "sqm.ge00.upload": "0",
+  "openwireless.use_before_last_reset": 100,
+  "openwireless.use_since_last_reset": 8000,
+  "openwireless.use_at_last_ui_reset": 90000
+}
+EOF
 cp local-lighttpd.pem $ETC/
 
 sed "s,REPO_DIR,$(readlink -f ..),; s,HTTP_PORT,$HTTP_PORT,; s,HTTPS_PORT,$HTTPS_PORT,;" local-lighttpd.conf > $ETC/local-lighttpd.conf
