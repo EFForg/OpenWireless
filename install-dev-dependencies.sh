298e7846e3ff4ecf2770933875bf43fb7477f42a
Andrew Kiellor <akiellor@gmail.com>
2014-11-22 02:04:01 -0800

    Add tftp to install-dev-dependencies.
    * tftp is required in dev for flashing the wndr3800

--- install-dev-dependencies.sh
+++ install-dev-dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 # On Ubuntu and similar systems, install packages that are necessary and/or
 # useful to build and debug OpenWireless.
-sudo apt-get install -y gettext unzip libncurses-dev subversion git inotify-tools lighttpd nodejs npm python2.7 python-pip
+sudo apt-get install -y gettext unzip libncurses-dev subversion git inotify-tools lighttpd nodejs npm python2.7 python-pip tftp
 cd $(dirname $0)
 pip install --user -qr requirements.txt
 npm install
