6e711fb8649b3c08727557f3dfadbe967c33a4bd
Andrew Kiellor <akiellor@gmail.com>
2014-11-23 01:22:50 -0800

    Add firefox/xvfb for running functional tests in virtual machine.
    * Also had to upgrade selenium version to support newer version of
    firefox.
    * Xvfb is a virtual framebuffer which allows running GUI applications
    without a real X windows environment.

--- install-dev-dependencies.sh
+++ install-dev-dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 # On Ubuntu and similar systems, install packages that are necessary and/or
 # useful to build and debug OpenWireless.
-sudo apt-get install -y gettext unzip libncurses-dev subversion git inotify-tools lighttpd nodejs npm python2.7 python-pip tftp libfontconfig
+sudo apt-get install -y gettext unzip libncurses-dev subversion git inotify-tools lighttpd nodejs npm python2.7 python-pip tftp libfontconfig firefox xvfb
 if [ ! -f /usr/bin/node ]; then
   sudo ln -s /usr/bin/nodejs /usr/bin/node
 fi
@@ -12,5 +12,7 @@ npm install
 if [ ! -f .git/hooks/pre-push ]; then
   ln -s ../../run-tests.sh .git/hooks/pre-push
 fi
+sudo cp infra/etc/init.d/Xvfb /etc/init.d/Xvfb
+sudo service Xvfb start
 echo "We strongly recommend adding the lines from ./ssh-config to your" \
      "SSH config. It will make developing on a router much faster."
