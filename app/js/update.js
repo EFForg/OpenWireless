52a987da0e266cf735c63b9c6967182476ea39d3
Sophie Krisch <skrisch@thoughtworks.com>
2014-11-25 11:50:39 -0800

    Fixed #220 [Sophie, Shane] Updating the last-checked-date both when you are already up to date and when you successfully download an update.

--- app/js/update.js
+++ app/js/update.js
@@ -2,6 +2,7 @@ var updateModule = (function(){
 
   var checkUpdateCallback = function(response){
     if(response.result.status == "up-to-date"){
+      dashboardModule.displayDate(new Date());
       alert("Your software is up-to-date!");
     } else {
       if(confirm("A new version is available. Would you like to update now?")){
@@ -10,14 +11,29 @@ var updateModule = (function(){
     }
   };
 
+  var errorCallback = function(response){
+    alert("Unable to update - check your internet connection");
+  };
+
+  var updateCallback = function(response){
+    if(response.result.status == "update-success"){
+      dashboardModule.displayDate(new Date());
+      alert("Successfully updated your software!");
+    } else {
+      alert("Unable to complete update.");
+    }
+  };
+
   var checkUpdateRequest = {
     "url": "/cgi-bin/routerapi/check_updates",
     "successCallback": checkUpdateCallback,
+    "errorCallback": errorCallback,
     "data": {}
   };
 
   var updateRequest = {
     "url": "/cgi-bin/routerapi/update",
+    "successCallback": updateCallback,
     "data": {}
   };
 
