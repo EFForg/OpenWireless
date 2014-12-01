65ea99a1d8c77af4bf14ea46768fbaf756435196
Andrew Kiellor <akiellor@gmail.com>
2014-11-30 23:18:18 -0800

    Should not restart wifi if disabled state does not change.

--- test/accumulate_bytes_test.py
+++ test/accumulate_bytes_test.py
@@ -10,6 +10,8 @@ sys.path.insert(0, os.path.join(
 import accumulate_bytes
 from fake_uci import FakeUci
 
+@mock.patch('accumulate_bytes.common.reset_wifi')
+@mock.patch('accumulate_bytes.uci', new_callable = FakeUci)
 class AccumulateBytesTest(unittest.TestCase):
     def with_usage_in_mb(self, uci, usage):
         uci.set("openwireless.use_before_last_reset", "0")
@@ -17,8 +19,6 @@ class AccumulateBytesTest(unittest.TestCase):
         uci.set("openwireless.use_at_last_ui_reset", "0")
         uci.commit("openwireless")
 
-    @mock.patch('accumulate_bytes.common.reset_wifi')
-    @mock.patch('accumulate_bytes.uci', new_callable = FakeUci)
     def test_turn_on_adapter_if_off_and_sufficient_bandwidth(self, uci, reset):
         uci.set("openwireless.maxmonthlybandwidth", "20")
         uci.set("wireless.@wifi-iface[1].disabled", "1")
@@ -30,8 +30,6 @@ class AccumulateBytesTest(unittest.TestCase):
         self.assertEquals(uci.get("wireless.@wifi-iface[1].disabled"), "0")
         reset.assert_called_with()
 
-    @mock.patch('accumulate_bytes.common.reset_wifi')
-    @mock.patch('accumulate_bytes.uci', new_callable = FakeUci)
     def test_turn_off_adapter_if_on_and_insufficient_bandwidth(self, uci, reset):
         self.with_usage_in_mb(uci, "20")
         uci.set("openwireless.maxmonthlybandwidth", "20")
@@ -43,3 +41,22 @@ class AccumulateBytesTest(unittest.TestCase):
         self.assertEquals(uci.get("wireless.@wifi-iface[1].disabled"), "1")
         reset.assert_called_with()
 
+    def test_do_not_reset_when_not_changing_on_state(self, uci, reset):
+        self.with_usage_in_mb(uci, "20")
+        uci.set("openwireless.maxmonthlybandwidth", "40")
+        uci.set("wireless.@wifi-iface[1].disabled", "0")
+        uci.commit("all")
+
+        accumulate_bytes.update_network_availability()
+
+        self.assertFalse(reset.called)
+
+    def test_do_not_reset_when_not_changing_off_state(self, uci, reset):
+        self.with_usage_in_mb(uci, "20")
+        uci.set("openwireless.maxmonthlybandwidth", "10")
+        uci.set("wireless.@wifi-iface[1].disabled", "1")
+        uci.commit("all")
+
+        accumulate_bytes.update_network_availability()
+
+        self.assertFalse(reset.called)
