019038e70a93dd922795acb50040ca3f72b8e840
Ranga Krishnan <rk@selwastor.com>
2014-12-10 21:25:51 -0800

    Removed python-mini-eff, explicitly create symlinks, clean up

--- build.sh
+++ build.sh
@@ -1,46 +1,32 @@
 #!/bin/bash
 
-  
   IMAGE="releases/openwireless-openwrt-squashfs-sysupgrade.bin"
 
   git submodule init
   git submodule update
 
   #build  router image
-  echo "building openwireless"
+  echo "Applying openwireless customizations"
+  #customize the file system
+  ./sendToBuild
 
-  #copy the config (kernel + packages)
-  cp OWrt/config-OWrt cerowrt/.config
-
+  echo "Setting up feeds"
   #setup the package building system
   cd cerowrt
-
   echo 'src-git cero https://github.com/dtaht/ceropackages-3.10' >> feeds.conf.default
-
-  ./scripts/feeds update 
-  #copy over python package
-  cp -r ../ow-python/python-mini-eff ./feeds/oldpackages/lang/
-  ./scripts/feeds update -a
-  #./scripts/feeds install libnettle
-  #./scripts/feeds install python-mini-eff
-  #./scripts/feeds install gnupg
-  #./scripts/feeds install tor 
+  ./scripts/feeds update
+  make package/symlinks
 
 
-
-  #customize the file system
+  echo "Building cerowrt"
+  #copy the config (kernel + packages)
+  cp ../OWrt/config-OWrt .config
+  make -j4
   cd ..
-  ./sendToBuild
-  cd cerowrt 
 
-  echo "building cerowrt"
-  make
-
-  cd ..
-  
+  echo "Build completed. Copying IMAGE to release directory" 
   mkdir -p releases
   cp cerowrt/bin/ar71xx/openwrt-ar71xx-generic-wndr3800-squashfs-sysupgrade.bin $IMAGE
-  
   if ! [ -f $IMAGE ]; then
     echo "ERROR: For some reason $IMAGE failed to generate!"
     exit 1
